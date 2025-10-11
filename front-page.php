13e1296754e542d841f9471f9649b2e613bedff8
DonartSefa <donart.sefa13@gmail.com>
2025-10-11 12:49:08 +0200

    8

---
+++ front-page.php
@@ -0,0 +1,14 @@
+
+<?php get_header(); ?>
+<main>
+   <section class="hero">
+    <img src="<?php echo esc_url(get_theme_file_uri('images/banner.png')) ?>" class="hero-banner" alt="">
+   </section>
+   <section class="features">
+        <h2 id="heading">Why choose us?</h2>
+   </section>
+</main>
+
+<?php get_footer(); ?>
+
+
